2dad05e21be1e15f09954b2236412f3c54f86a52
akina-ne <e1b23001@oit.ac.jp>
2025-10-28 14:29:19 +0900

    [200~Sample5-1:sample51.htmlを表示するGETリクエストを実装する

---
+++ src/main/java/oit/is/kadai5/dbsql2/controller/Sample51Controller.java
@@ -0,0 +1,21 @@
+package oit.is.kadai5.dbsql2.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/sample5")
+public class Sample51Controller {
+  @GetMapping("step1")
+  public String sample51() {
+    return "sample51.html";
+  }
+}
