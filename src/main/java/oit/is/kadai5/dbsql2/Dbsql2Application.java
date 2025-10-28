4aff1571e4b7168b0ce6e7db96ccb86d371066d8
meganedayo <e1b23021@oit.ac.jp>
2025-10-28 13:35:30 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/kadai5/dbsql2/Dbsql2Application.java
@@ -0,0 +1,13 @@
+package oit.is.kadai5.dbsql2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dbsql2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dbsql2Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/kadai5/dbsql2/Dbsql2ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.kadai5.dbsql2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dbsql2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
