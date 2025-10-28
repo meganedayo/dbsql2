c8b9f9d8a35f723726d815c9a399a9d2e25fac9a
meganedayo <e1b23021@oit.ac.jp>
2025-10-28 13:56:20 +0900

    認可処理を実装

--- src/main/java/oit/is/kadai5/dbsql2/security/Sample3AuthConfiguration.java
+++ src/main/java/oit/is/kadai5/dbsql2/security/Sample3AuthConfiguration.java
@@ -13,6 +13,34 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class Sample3AuthConfiguration {
   /**
+   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
+   *
+   * @param http
+   * @return
+   * @throws Exception
+   */
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.formLogin(login -> login
+        .permitAll())
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")) // ログアウト後に / にリダイレクト
+        .authorizeHttpRequests(authz -> authz
+            .requestMatchers("/sample3/**").authenticated() // /sample3/以下は認証済みであること
+            .requestMatchers("/sample4/**").authenticated() // /sample4/以下は認証済みであること
+            .requestMatchers("/sample5/**").authenticated() // /sample5/以下は認証済みであること
+            .requestMatchers("/sample58*").authenticated() // /sample58*は認証済みであること
+            .anyRequest().permitAll()) // 上記以外は全員アクセス可能
+        .csrf(csrf -> csrf
+            .ignoringRequestMatchers("/h2-console/*", "/sample2*/**")) // sample2用にCSRF対策を無効化
+        .headers(headers -> headers
+            .frameOptions(frameOptions -> frameOptions
+                .sameOrigin()));
+    return http.build();
+  }
+
+  /**
    * 認証処理に関する設定（誰がどのようなロールでログインできるか）
    *
    * @return
