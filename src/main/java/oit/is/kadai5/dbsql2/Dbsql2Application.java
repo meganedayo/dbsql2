184a3081d78c14d1cdd680af42eb7dc824c880b4
e1b23017 <e1b23017@oit.ac.jp>
2025-10-28 16:39:16 +0900

    Sample5-6-1:ブラウザで指定のページを開いている限りカウントアップする数字を表示する処理を実装する

--- src/main/java/oit/is/kadai5/dbsql2/Dbsql2Application.java
+++ src/main/java/oit/is/kadai5/dbsql2/Dbsql2Application.java
@@ -2,12 +2,16 @@ package oit.is.kadai5.dbsql2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class Dbsql2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Dbsql2Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Dbsql2Application.class, args);
+  }
 
 }
